85a2285c49ea2c7d5d7146f9b14c50e3da0edb44
John Stassen <john.stassen@gmail.com>
2020-08-26 14:52:28 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,4 +1,32 @@
 class CashRegister
+  attr_accessor :total
+  
+  def initialize(discount = nil)
+    @total = 0
+    @items = []
+    @discount = discount
+  end
 
+  def add_item(title, price, quantity = 1)
+    @old_total = @total
+    @total = @total + (quantity * price)
+    @items << title
+  end
+
+  def apply_discount
+    if @discount = nil
+      return "There is no discount to apply"
+    else
+      @total = @total * (1-(@discount / 100))
+    end
+  end
+
+  def items
+    @items
+  end
+
+  def void_last_transaction
+    @items.pop
+    @total = @old_total
 
 end
